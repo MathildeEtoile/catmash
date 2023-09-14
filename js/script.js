6abccbd3802da92b29feebcc848e87a01fbd52ce
Mathilde Allain <mathilde.allain@datadoghq.com>
2023-09-14 08:38:40 +0200

    Get and show all cats in list page with votes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 let totalVotes = sessionStorage.getItem("total-votes") || 0;
 
+let cats;
+
+$.get("https://latelier.co/data/cats.json?callback=?", function(data){
+	cats = data.images;
+	$.each( cats, function( _, val ) {
+		nbVotes = sessionStorage.getItem(val.url) || 0;
+		$( ".list" ).append( 
+			`<figure id=${val.id}>
+				<div><img src="${val.url}" alt="cat"></div>
+				<figcaption>${nbVotes}${nbVotes < 2 ? ' vote' : ' votes'}</figcaption>
+			</figure>` 
+		);
+		$( `#${val.id}` ).css("order", -nbVotes);
+	});
+});
+
 $(document).ready(function(){
 	$('.total-votes').text(`${totalVotes}${totalVotes < 2 ? ' vote' : ' votes'}`);	
 });
